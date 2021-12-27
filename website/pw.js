9df102cadb41ab0ff75da5d739eda79871c61b9d
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-27 12:59:42 -0500

    Create pw.js

---
+++ website/pw.js
@@ -0,0 +1,13 @@
+document.getElementById("check").addEventListener("click",async function(){
+  var pw = {}
+  var randID = Math.round(Math.random()*1000000)
+  pw[randID] = document.getElementById("pwrd").value
+  var badlist = await (await fetch("https://raw.githubusercontent.com/iam-py-test/password-check/main/data/worst_password.txt")).text().split("\n")
+  if(badlist.includes(pw[randID])){
+    document.getElementById("isinbad").textContent = "Your password is in the list of worst passwords"
+  }
+  else{
+    document.getElementById("isinbad").textContent = "Your password is not in the list of worst passwords"
+  }
+})
+
