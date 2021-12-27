8642a846a7326f605f4c0396724dd81fc9565a03
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-27 13:27:35 -0500

    Uh oh

--- website/pw.js
+++ website/pw.js
@@ -2,7 +2,8 @@ document.getElementById("check").addEventListener("click",async function(){
   var pw = {}
   var randID = Math.round(Math.random()*1000000)
   pw[randID] = document.getElementById("pwrd").value
-  var badlist = await (await fetch("https://raw.githubusercontent.com/iam-py-test/password-check/main/data/worst_password.txt")).text().split("\n")
+  var badlist = await (await fetch("https://raw.githubusercontent.com/iam-py-test/password-check/main/data/worst_password.txt")).text()
+  badlist = badlist.split("\n")
   if(badlist.includes(pw[randID])){
     document.getElementById("isinbad").textContent = "Your password is in the list of worst passwords"
   }
